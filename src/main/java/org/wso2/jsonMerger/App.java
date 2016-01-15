09c5485645af8a8e026e21513320db8f5d948abb
Randika Navagamuwa <randika.12@cse.mrt.ac.lk>
2016-01-15 17:06:35 +0530

    Input size can be dynamic
    Now user doesn't have to give how many swagger files are there as a
    parameter. It automatically identifies that. All you have to do is place
    all the swagger files inside inputSwagger folder

--- src/main/java/org/wso2/jsonMerger/App.java
+++ src/main/java/org/wso2/jsonMerger/App.java
@@ -1,7 +1,12 @@
 package org.wso2.jsonMerger;
 
+import java.io.File;
+
 public class App {
+	
 	public static void main(String[] args) {
-		merger.merge(2);
+		merger.merge();
+		
 	}
+	
 }
--- src/main/java/org/wso2/jsonMerger/merger.java
+++ src/main/java/org/wso2/jsonMerger/merger.java
@@ -1,5 +1,6 @@
 package org.wso2.jsonMerger;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
@@ -13,42 +14,63 @@ import com.wordnik.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
 
 public class merger {
-	public static void merge(int swaggerCount) {
-		Swagger initial = new SwaggerParser().read("inputSwagger/0.json");
-		Map<String, Path> paths = null;
-		Map<String, Path> tempPaths;
-		Map<String, Model> definitions = null;
-		Map<String, Model> tempDefinitions;
-		
-		for(int i=1;i<swaggerCount;i++){
-			Swagger temp = new SwaggerParser().read("inputSwagger/"+i+".json");
+	private static int swaggerCount = 0;
 
-			paths = initial.getPaths();
-			tempPaths = temp.getPaths();
-			for (Entry<String, Path> entryPath : tempPaths.entrySet()) {
-				paths.put(entryPath.getKey(), entryPath.getValue());
+	public static void merge() {
+		getCount("inputSwagger");
+		if (swaggerCount > 0) {
+			Swagger initial = new SwaggerParser().read("inputSwagger/0.json");
+			Map<String, Path> paths = null;
+			Map<String, Path> tempPaths;
+			Map<String, Model> definitions = null;
+			Map<String, Model> tempDefinitions;
+
+			for (int i = 1; i < swaggerCount; i++) {
+				Swagger temp = new SwaggerParser().read("inputSwagger/" + i + ".json");
+
+				paths = initial.getPaths();
+				tempPaths = temp.getPaths();
+				for (Entry<String, Path> entryPath : tempPaths.entrySet()) {
+					paths.put(entryPath.getKey(), entryPath.getValue());
+				}
+
+				definitions = initial.getDefinitions();
+				tempDefinitions = temp.getDefinitions();
+				for (Entry<String, Model> entryDef : tempDefinitions.entrySet()) {
+					definitions.put(entryDef.getKey(), entryDef.getValue());
+				}
 			}
 
-			definitions = initial.getDefinitions();
-			tempDefinitions = temp.getDefinitions();
-			for (Entry<String, Model> entryDef : tempDefinitions.entrySet()) {
-				definitions.put(entryDef.getKey(), entryDef.getValue());
+			initial.setPaths(paths);
+			initial.setDefinitions(definitions);
+			Gson gson = new Gson();
+			String json = gson.toJson(initial);
+
+			try {
+				// write converted json data to a file named "file.json"
+				FileWriter writer = new FileWriter("swagger.json");
+				writer.write(json);
+				writer.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-		
-		initial.setPaths(paths);
-		initial.setDefinitions(definitions);
-		Gson gson = new Gson();
-		String json = gson.toJson(initial);
 
-		try {
-			// write converted json data to a file named "file.json"
-			FileWriter writer = new FileWriter("swagger.json");
-			writer.write(json);
-			writer.close();
+	}
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void getCount(String dirPath) {
+		File f = new File(dirPath);
+		File[] files = f.listFiles();
+
+		if (files != null)
+			for (int i = 0; i < files.length; i++) {
+				swaggerCount++;
+				File file = files[i];
+
+				if (file.isDirectory()) {
+					getCount(file.getAbsolutePath());
+				}
+			}
 	}
 }
