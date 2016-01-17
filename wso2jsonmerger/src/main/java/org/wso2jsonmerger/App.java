efb75454db9ca3497733f31348de54bcc7eaf039
Randika Navagamuwa <randika.12@cse.mrt.ac.lk>
2016-01-17 11:41:43 +0530

    Readme changed

--- wso2jsonmerger/src/main/java/org/wso2jsonmerger/App.java
+++ wso2jsonmerger/src/main/java/org/wso2jsonmerger/App.java
@@ -1,13 +1,14 @@
 package org.wso2jsonmerger;
 
-/**
- * Hello world!
- *
- */
+import com.google.gson.Gson;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
 public class App 
 {
     public static void main( String[] args )
     {
         merger.merge();
+//    	Swagger initial = new SwaggerParser().read("inputSwagger/0.json");
+//    	System.out.print(initial.getDefinitions().get("Pet"));
     }
 }
